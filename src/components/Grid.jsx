42079382f6c43c5331dcd70ba40ce2623aa1f85f
saraanastazjaosiecka <sara.anastazja.osiecka@gmail.com>
2023-06-04 18:40:50 +0200

    Adding all grid buttons & preparing scss partials files for styling

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import "../scss/Grid_Styles.scss";
 
 export default function Grid({ onColorSelected }) {
   //
@@ -16,31 +15,97 @@ export default function Grid({ onColorSelected }) {
   }
 
   return (
-    <div>
-      <button className="grid_button_blue" onClick={() => selectColor("blue")}>
-        blue
-      </button>
-      <button className="grid_button_red" onClick={() => selectColor("red")}>
-        red
-      </button>
-      <button
-        className="grid_button_green"
-        onClick={() => selectColor("green")}
-      >
-        green
-      </button>
-      <button
-        className="grid_button_yellow"
-        onClick={() => selectColor("yellow")}
-      >
-        yellow
-      </button>
-      <br />
-      <br />
+    <>
+      <h1> What color matches your current mood? </h1>
+      <div className="grid_buttons">
+        <button
+          className="grid_button_C_major"
+          onClick={() => selectColor("#ff1d04")}
+        >
+          C major
+        </button>
+
+        <button
+          className="grid_button_G_major"
+          onClick={() => selectColor("#fb9507")}
+        >
+          G major
+        </button>
+        <button
+          className="grid_button_D_major"
+          onClick={() => selectColor("#fbff00")}
+        >
+          D major
+        </button>
+
+        <button
+          className="grid_button_A_major"
+          onClick={() => selectColor("#4dd34d")}
+        >
+          A major
+        </button>
+
+        <button
+          className="grid_button_E_major"
+          onClick={() => selectColor("#cef2f4")}
+        >
+          E major
+        </button>
+
+        <button
+          className="grid_button_B_major"
+          onClick={() => selectColor("#86d3ff")}
+        >
+          B major
+        </button>
+
+        <button
+          className="grid_button_Gb_major"
+          onClick={() => selectColor("#83a6f2")}
+        >
+          F#/Gb
+        </button>
+
+        <button
+          className="grid_button_Db_major"
+          onClick={() => selectColor("#964df5")}
+        >
+          Db major
+        </button>
+
+        <button
+          className="grid_button_Ab_major"
+          onClick={() => selectColor("#ca90f0")}
+        >
+          Ab major
+        </button>
+
+        <button
+          className="grid_button_Eb_major"
+          onClick={() => selectColor("#df519a")}
+        >
+          Eb major
+        </button>
+
+        <button
+          className="grid_button_Bb_major"
+          onClick={() => selectColor("#b48295")}
+        >
+          Bb major
+        </button>
+
+        <button
+          className="grid_button_F_major"
+          onClick={() => selectColor("#b73939")}
+        >
+          F major
+        </button>
+      </div>
+
       <button onClick={handleTransferToHistory}>
         {" "}
         Read your gratitude journal{" "}
       </button>
-    </div>
+    </>
   );
 }
--- src/components/Journal_Form.jsx
+++ src/components/Journal_Form.jsx
@@ -4,19 +4,41 @@ import supabase from "../services/supabase";
 
 // // Array with songs
 const songs = {
-  blue: {
-    links: [
-      " https://open.spotify.com/track/24nTZUgqU1P7fn074Pm3xZ?si=2508a1a9bcd24507",
-      "https://open.spotify.com/track/24nTZUgqU1P7fn074Pm3xZ?si=2508a1a9bcd24507",
-      "https://open.spotify.com/track/24nTZUgqU1P7fn074Pm3xZ?si=2508a1a9bcd24507",
-      "https://open.spotify.com/track/24nTZUgqU1P7fn074Pm3xZ?si=2508a1a9bcd24507",
-    ],
+  "#ff1d04": {
+    links: ["C major 1", " C major 2", " C major 3", "C major 4"],
   },
-  red: {
-    links: ["red 1", " red 2", " red 3", "red 4"],
+  "#fb9507": {
+    links: ["G major 1 ", " G major 2", "G major 3", "G major 4"],
   },
-  green: {
-    links: ["green 1", " green 2", " green 3", "green 4"],
+  "#fbff00": {
+    links: ["D major 1", "D major 2", "D major 3", "D major 4"],
+  },
+  "#4dd34d": {
+    links: [" A major 1", " A major 2", " A major 3", "A major 4"],
+  },
+  "#cef2f4": {
+    links: ["E major 1", " E major  2", " E major  3", "E major  4"],
+  },
+  "#86d3ff": {
+    links: ["B major 1", " B major 2", " B major 3", "B major 4"],
+  },
+  "#83a6f2": {
+    links: ["Gb major 1", " Gb major 2", " Gb major 3", "Gb major 4"],
+  },
+  "#964df5": {
+    links: ["Db major 1", " Db major 2", " Db major 3", "Db major 4"],
+  },
+  "#ca90f0": {
+    links: ["Ab major 1", " Ab major 2", " Ab major 3", "Ab major 4"],
+  },
+  "#df519a": {
+    links: [" Eb major 1", "  Eb major 2", "  Eb major 3", " Eb major 4"],
+  },
+  "#b48295": {
+    links: ["  Bb major 1", "   Bb major 2", "   Bb major 3", "  Bb major 4"],
+  },
+  "#b73939": {
+    links: [" F major 1", "  F major 2", "  F major 3", " F major 4"],
   },
 };
 
